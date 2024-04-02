6acabffd8a8c9eb87558eebbb93bb2a1d8c37045
Charlles Abreu <craabreu@gmail.com>
2024-04-02 09:52:16 -0400

    Initial extended space context implementation

--- openxps/__init__.py
+++ openxps/__init__.py
@@ -3,5 +3,9 @@ Extended Phase-Space Methods with OpenMM
 """
 
 from ._version import __version__  # noqa: F401
+from .extended_context import (  # noqa: F401
+    ExtendedSpaceContext,
+    ExtendedSpaceIntegrator,
+)
 from .extra_dof import ExtraDOF  # noqa: F401
 from .systems import PhysicalSystem  # noqa: F401
