047f250c7f910deda0abe8fdb1385f37a5be12c0
Charlles Abreu <craabreu@gmail.com>
2024-04-03 19:42:05 -0400

    Fixed bug

--- openxps/__init__.py
+++ openxps/__init__.py
@@ -3,6 +3,5 @@ Extended Phase-Space Methods with OpenMM
 """
 
 from ._version import __version__  # noqa: F401
-from .extended_context import ExtendedSpaceContext  # noqa: F401
+from .context import ExtendedSpaceContext  # noqa: F401
 from .extra_dof import ExtraDOF  # noqa: F401
-from .systems import PhysicalSystem  # noqa: F401
